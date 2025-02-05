470b431507c2e108be2462140cb4128675c2fbb4
ATZ Mughal <atzmughal@gmail.com>
2025-02-05 19:58:58 +0500

    Update info.py

--- info.py
+++ info.py
@@ -57,7 +57,7 @@ SUPPORT_CHAT = environ.get(
 )  # Support group link ( make sure bot is admin )
 # ---------------------------------------------------------------
 # ---------------------------------------------------------------
-IS_VERIFY = is_enabled("IS_VERIFY", False)
+IS_VERIFY = is_enabled("IS_VERIFY", True)
 # ---------------------------------------------------------------
 TUTORIAL = environ.get("TUTORIAL", "") #VERIFICATION TUTORIAL VIDEO URL, REPLACE ALL WITH YOURS
 VERIFY_IMG = environ.get(
